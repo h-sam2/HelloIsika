560e6319b9b1ac17847266f1fe520ca174af2005
Jonathan <jonathan.grimm@dimaconsulting.fr>
2019-02-27 12:11:27 +0100

    first commit

---
+++ HelloIsika/src/fr/isika/servlet/HelloIsikaServlet.java
@@ -0,0 +1,44 @@
+package fr.isika.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloIsikaServlet
+ */
+@WebServlet("/HelloIsika")
+public class HelloIsikaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public HelloIsikaServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 PrintWriter out = response.getWriter();
+         out.print("<html><body><h1 align='center'> Bonjour Isika ! </h1></body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
